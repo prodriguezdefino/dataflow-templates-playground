79d5788b73b9319f5fa07dd29643c29e547e63ba
prodriguezdefino <prodriguezdefino@gmail.com>
2022-06-30 10:16:01 -0700

    changed impl of the custom udf

--- udf-impl/src/main/java/com/google/cloud/pso/dataflow/udf/CustomUDF.java
+++ udf-impl/src/main/java/com/google/cloud/pso/dataflow/udf/CustomUDF.java
@@ -1,20 +1,23 @@
 package com.google.cloud.pso.dataflow.udf;
 
+import java.util.Optional;
 import org.apache.beam.sdk.values.Row;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.joda.time.Instant;
 
 /**
- *
+ * A custom UDF that decides on a positive sentiment based on the page score. 
  */
 public class CustomUDF implements UDF {
 
-  private static final Logger LOG = LoggerFactory.getLogger(CustomUDF.class);
-
   @Override
   public Row apply(Row row) throws RuntimeException {
-    LOG.info("just logging for now");
-    return UDF.super.apply(row); 
+    
+    return Row.fromRow(row)
+            .withFieldValue("sentiment", 
+                    Optional.of(row.getInt32("page_score"))
+                            .map(score -> score > 5 ? "positive" : "negative").get())
+            .withFieldValue("processing_time", Instant.now().toString())
+            .build();
   }
 
 }
